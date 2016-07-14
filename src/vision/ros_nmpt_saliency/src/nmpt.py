fbf3605c16c842a6e79e6fce43cf657500c06fd9
natnaelargaw <natnaelargaw@gmail.com>
2016-07-14 12:50:06 +0300

    NMPT based Servoing Enabled. AtomSpace Manipulation TBD

--- src/vision/ros_nmpt_saliency/src/nmpt.py
+++ src/vision/ros_nmpt_saliency/src/nmpt.py
@@ -1,72 +1,68 @@
 #!/usr/bin/env python
-from collections import deque
+
 import rospy
-import math
-import time
-from std_msgs.msg import String
+
 from blender_api_msgs.msg import Target
 from ros_nmpt_saliency.msg import targets
 from pi_face_tracker.msg import FaceEvent, Faces
 
 '''
-    This is a sample subscriber written to get sudden change msgs from /opencog/suddenchange topic.
- '''
-class suddenChange:
-
+    This is a sample subscriber written to control Sophia's Head movement based on the degree of Salient points.
+'''
+class NMPT_SERVO:
   def __init__(self):
     self.GLOBAL_FLAG = 0
     self.TOPIC_FACE_TARGET = "/blender_api/set_face_target"
+    self.TOPIC_GAZE_TARGET = "/blender_api/set_gaze_target"
+    self.TOPIC_SALIENT_POINT = "/nmpt_saliency_point"
     self.TOPIC_FACE_EVENT = "/camera/face_event"
+    
     self.EVENT_NEW_FACE = "new_face"
     self.EVENT_LOST_FACE = "lost_face"
-    # Overrides current face being tracked by WebUI
     self.EVENT_TRACK_FACE = "track_face"
 
-    self.audoSub = rospy.Subscriber("/nmpt_saliency_point", targets, self.GetAudioClass)
+    self.salience = rospy.Subscriber(self.TOPIC_SALIENT_POINT, targets, self.lookat_salientP_cb)
     self.look_pub = rospy.Publisher(self.TOPIC_FACE_TARGET, Target, queue_size=10)
+    self.look_gaze = rospy.Publisher(self.TOPIC_GAZE_TARGET, Target, queue_size=10)
     rospy.Subscriber(self.TOPIC_FACE_EVENT, FaceEvent, self.face_event_cb)
 
-
-  # Callback function
-  def GetAudioClass(self, data):
+  # Callback for NMPT_B
+  def lookat_salientP_cb(self, data):
     loc = []
     loc = data.positions[0]
     degree = data.degree 
     t = Target()
-    
-    t.x = loc.z + 0.5
+    t.x = 1.0
     if loc.x <= 0.5:
-    	loc.x =(0.5-loc.x) *2
+    	loc.x =(0.5 - loc.x)
     else:
-        loc.x =(loc.x -0.5) * -2
+        loc.x =(0.5 -loc.x)
     t.y = loc.x
     if loc.y <= 0.5:
-    	loc.y =(0.5-loc.y)*2
+    	loc.y =(0.5 - loc.y)
     else:
-        loc.y =(loc.y -0.5) * -2
-
+        loc.y =(0.5 -loc.y)
     t.z = loc.y
 
-    if self.GLOBAL_FLAG and degree >=8:
+    if self.GLOBAL_FLAG and degree >=5:
         self.look_pub.publish(t)
-    print(t)
+        self.look_gaze.publish(t)
 
   def face_event_cb(self, data):
     if data.face_event == self.EVENT_NEW_FACE:
+	print ("Controling pkg: PyFace Tracker")
 	self.GLOBAL_FLAG = 0
     elif data.face_event == self.EVENT_LOST_FACE:
+        print ("Controling pkg: Salience Detector")
 	self.GLOBAL_FLAG = 1
     elif data.face_event == self.EVENT_TRACK_FACE:
 	pass
 
-
-
 if __name__ == '__main__':
     global d
-
     try:
-        rospy.init_node('suddenChange', anonymous=True)
-        suddenChange()
+        rospy.init_node('NMPT_SERVO', anonymous=True)
+        NMPT_SERVO()
         rospy.spin()
     except rospy.ROSInterruptException as e:
         print(e)

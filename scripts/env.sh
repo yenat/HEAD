ff9806c6bef2fc0d6c6353f1df62c727c7b87b79
yenat <yenatshif@gmail.com>
2016-07-26 16:51:43 +0300

    Added psi-rules

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,1 +1,27 @@
 export HR_WORKSPACE=/home/yenat/hansonrobotics
+export HR_VERSION=0.1.0
+export HR_ENVFILE_PATH=/home/yenat/.hr/env.sh
+export HR_PREFIX=/opt/hansonrobotics
+export HR_CACHE=/home/yenat/.hr/cache
+
+export VISION_TOOL_PREFIX=/opt/hansonrobotics/vision
+export DLIB_DIR=/opt/hansonrobotics/vision/dlib
+export TORCH_DIR=/opt/hansonrobotics/vision/torch
+export OPENFACE_DIR=/opt/hansonrobotics/vision/openface
+export CPPMT_DIR=/opt/hansonrobotics/vision/CppMT
+export EMOTIME_DIR=/opt/hansonrobotics/vision/emotime
+
+export MARKY_MARKOV_DIR=/opt/hansonrobotics/marky_markov
+export HR_MODELS=/home/yenat/.hr/models
+
+export ROS_LOG_DIR="/home/yenat/.hr/log"
+export OCBHAVE="/home/yenat/hansonrobotics/opencog/ros-behavior-scripting"
+export PYTHONPATH=/home/yenat/hansonrobotics/HEAD/devel/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages:/usr/local/share/opencog/python:/home/yenat/Opencog/opencog/build/opencog/cython:/usr/local/share/opencog/python/opencog:/usr/bin/python:/home/yenat/Opencog/opencog/opencog/python:/home/yenat/Opencog/opencog/opencog/python/web/api:/src:/opt/hansonrobotics/vision/openface:/opt/hansonrobotics/vision/dlib/dlib-19.0/dist:/usr/local/share/opencog/python
+
+export LD_LIBRARY_PATH=/home/yenat/hansonrobotics/HEAD/devel/lib:/opt/ros/indigo/lib:/opt/hansonrobotics/vision/torch/install/lib:/home/yenat/distro/install/lib:/opt/hansonrobotics/vision/torch/install/lib:/home/yenat/distro/install/lib:/opt/hansonrobotics/vision/CppMT:/opt/hansonrobotics/vision/emotime/build/src
+export LIBRARY_PATH=:/opt/hansonrobotics/vision/CppMT:/opt/hansonrobotics/vision/emotime/build/src
+export DLIB_PATH=/opt/hansonrobotics/vision/dlib/dlib-19.0
+export CPLUS_INCLUDE_PATH=:/opt/hansonrobotics/vision/CppMT:/opt/hansonrobotics/vision/emotime/include:/opt/hansonrobotics/vision/emotime/src/facedetector:/opt/hansonrobotics/vision/emotime/src/utils:/opt/hansonrobotics/vision/emotime/src/gaborbank:/opt/hansonrobotics/vision/emotime/src/detector:/opt/hansonrobotics/vision/emotime/src/training:
+export MANYEARSLIB_PREFIX=/opt/hansonrobotics/manyears-C-1.0.0
+
+source /opt/hansonrobotics/vision/torch/install/bin/torch-activate
